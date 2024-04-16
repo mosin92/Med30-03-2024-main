9bba254584cd737e219918ce680e64eb41f9a673
Mohsin <55811423+mosin92@users.noreply.github.com>
2024-04-16 23:14:26 +0500

    update code

---
+++ src/SongsLists/SongsLlegenda.js
@@ -0,0 +1,46 @@
+export const songsList = [
+  {
+    title: 'El Timbaler del Bruc',
+    artist: 'kpimarketing.es',
+    flag: '333',
+    artwork: require('../images/timbaler4.webp'),
+    url: require('../audios/Timbaler.mp3'),
+    id: '11',
+  },
+
+  {
+    title: 'El Tamborilero del Bruc',
+    artist: 'kpimarketing.es',
+    flag: '777',
+    artwork: require('../images/timbaler4.webp'),
+    url: require('../audios/Cast_LeyendaTimbaler.mp3'),
+    id: '12',
+  },
+
+  {
+    title: 'Manresa el misteri de la llum',
+    artist: 'Alba Subirà Fernández',
+    flag: '333',
+    artwork: require('../images/Manresa_foto_new.webp'),
+    url: require('../audios/Manresa.mp3'),
+    id: '13',
+  },
+
+  {
+    title: 'La Llegenda de Sant Jordi',
+    artist: 'Octavi Pujades',
+    flag: '333',
+    artwork: require('../images/jordi_new.jpg'),
+    url: require('../audios/jordi_catala1.mp3'),
+    id: '14',
+  },
+
+  {
+    title: 'La Leyenda de Sant Jordi',
+    artist: 'Octavi Pujades',
+    flag: '777',
+    artwork: require('../images/jordi.jpg'),
+    url: require('../audios/jordi_cast1.mp3'),
+    id: '15',
+  },
+];
